0aad25902f57ffc63052f88f210de900225676df
Tom Buscher <tom@storycloud.co>
2015-07-10 09:10:53 -0700

    Add OpenVPN Access Server to bastion host

---
+++ site-cookbooks/openvpnas_server/metadata.rb
@@ -0,0 +1,9 @@
+name             'openvpnas_server'
+maintainer       'StoryCloud Inc'
+maintainer_email 'dev@storycloud.co'
+license          'All rights reserved'
+description      'Installs/Configures OpenVPN Access Server'
+long_description 'Installs/Configures OpenVPN Access Server'
+version          '0.1.0'
+
+depends "openvpnas"
