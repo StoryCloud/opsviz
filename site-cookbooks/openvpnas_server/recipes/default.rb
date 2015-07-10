645708d6318c7064f851a2c77828fac451665e67
Tom Buscher <tom@storycloud.co>
2015-07-10 11:59:29 -0700

    Don't configure the OpenVPN server admin password after all.
    It's stored in the clear in the OpsWorks custom JSON. For now we'll just
    configure it by hand after installation.

--- site-cookbooks/openvpnas_server/recipes/default.rb
+++ site-cookbooks/openvpnas_server/recipes/default.rb
@@ -1,12 +1,3 @@
 node.normal[:openvpnas][:url] = "http://swupdate.openvpn.org/as/openvpn-as-#{node[:openvpnas_server][:version]}-Ubuntu14.amd_64.deb"
 
 include_recipe "openvpnas::default"
-
-Chef::Application.fatal!("OpenVPN admin password must be set") unless node[:openvpnas_server][:admin_password].is_a?(String)
-
-chef_gem "ruby-shadow"
-
-user "openvpn" do
-  action :modify
-  password node[:openvpnas_server][:admin_password]
-end
