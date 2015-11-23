df32c58776c068f8764abd9e9876b590b247d7b7
Tom Buscher <tom@storycloud.co>
2015-11-23 11:15:57 -0800

    Turn off some ElasticSearch Sensu checks

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -47,44 +47,44 @@ sensu_check "check-graphite-cache" do
   additional(:occurrences => 2)
 end
 
-sensu_check "check-es-heap" do
-  command "check-es-heap.rb -h #{node[:kibana][:elasticsearch_server]}"
-  handlers node[:bb_monitor][:sensu][:default_check_handlers]
-  subscribers ["dashboard"]
-  interval 300
-  additional(:occurrences => 2)
-end
+# sensu_check "check-es-heap" do
+#   command "check-es-heap.rb -h #{node[:kibana][:elasticsearch_server]}"
+#   handlers node[:bb_monitor][:sensu][:default_check_handlers]
+#   subscribers ["dashboard"]
+#   interval 300
+#   additional(:occurrences => 2)
+# end
 
-sensu_check "check-es-cluster-status" do
-  command "check-es-cluster-status.rb -h #{node[:kibana][:elasticsearch_server]}"
-  handlers node[:bb_monitor][:sensu][:default_check_handlers]
-  subscribers ["dashboard"]
-  interval 300
-  additional(:occurrences => 2)
-end
+# sensu_check "check-es-cluster-status" do
+#   command "check-es-cluster-status.rb -h #{node[:kibana][:elasticsearch_server]}"
+#   handlers node[:bb_monitor][:sensu][:default_check_handlers]
+#   subscribers ["dashboard"]
+#   interval 300
+#   additional(:occurrences => 2)
+# end
 
-sensu_check "check-es-file-descriptors" do
-  command "check-es-file-descriptors.rb -h #{node[:kibana][:elasticsearch_server]}"
-  handlers node[:bb_monitor][:sensu][:default_check_handlers]
-  subscribers ["dashboard"]
-  interval 300
-  additional(:occurrences => 2)
-end
+# sensu_check "check-es-file-descriptors" do
+#   command "check-es-file-descriptors.rb -h #{node[:kibana][:elasticsearch_server]}"
+#   handlers node[:bb_monitor][:sensu][:default_check_handlers]
+#   subscribers ["dashboard"]
+#   interval 300
+#   additional(:occurrences => 2)
+# end
 
-stack_name = node[:opsworks][:stack][:name].downcase.gsub(' ','_')
-# Metrics
-sensu_check "metric-cluster-elasticsearch" do
-  type "metric"
-  command "es-cluster-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch.cluster"
-  handlers node[:bb_monitor][:sensu][:default_metric_handlers]
-  subscribers ["dashboard"]
-  interval 60
-end
+# stack_name = node[:opsworks][:stack][:name].downcase.gsub(' ','_')
+# # Metrics
+# sensu_check "metric-cluster-elasticsearch" do
+#   type "metric"
+#   command "es-cluster-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch.cluster"
+#   handlers node[:bb_monitor][:sensu][:default_metric_handlers]
+#   subscribers ["dashboard"]
+#   interval 60
+# end
 
-sensu_check "metric-node-elasticsearch" do
-  type "metric"
-  command "es-node-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch"
-  handlers node[:bb_monitor][:sensu][:default_metric_handlers]
-  subscribers ["dashboard"]
-  interval 60
-end
+# sensu_check "metric-node-elasticsearch" do
+#   type "metric"
+#   command "es-node-metrics.rb -h #{node[:kibana][:elasticsearch_server]} -s stats.#{stack_name}.elasticsearch"
+#   handlers node[:bb_monitor][:sensu][:default_metric_handlers]
+#   subscribers ["dashboard"]
+#   interval 60
+# end
